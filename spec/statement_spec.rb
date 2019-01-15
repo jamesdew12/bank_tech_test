22d98c00fa022a677914d0aee200257b4936e541
James de Winton <james.dewinton@gmail.com>
2019-01-15 11:55:04 +0000

    more tests

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -5,5 +5,10 @@ require "statement"
 describe Statement do
   it "prints statement" do
   end
+  it "card can prints_statement nothing if no transactions" do
+    expect(subject.print).to eq nil
+
+  end
+
 
 end
